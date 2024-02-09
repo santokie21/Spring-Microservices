916a591f3d9f8109147c4908e4993b24941e8fa9
santokie21 <officialuse3012@gmail.com>
2024-02-09 19:13:04 +0530

    initial commit

---
+++ src/main/java/io/noobi/jobapp/JobAppApplication.java
@@ -0,0 +1,13 @@
+package io.noobi.jobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/noobi/jobapp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.noobi.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
